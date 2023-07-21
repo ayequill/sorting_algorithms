12af86104dee7cb936dfe8c2c9e398825ea5c4e1
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-07-21 00:57:12 +0000

    added selection sort prototype

--- sort.h
+++ sort.h
@@ -21,4 +21,5 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void swap_integers(int *n, int *m);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 #endif
